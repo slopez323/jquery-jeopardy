caf4920c6fb67e1d8fe3100100056ce1196a62e1
Sarah Lopez <slopez323@gmail.com>
2022-04-18 18:51:01 -0400

    by show and cat

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
+let showNum;
+let showList = JSON.parse(localStorage.getItem('showList')) || [];
 let pickedQuestions = JSON.parse(localStorage.getItem('list')) || [];
 let pickedCells = JSON.parse(localStorage.getItem('picked')) || [];
 let score = localStorage.getItem('score') || 0;
+let categories = [];
 
-function setup() {
+setup();
+
+async function setup() {
+    await getRandomShowNum();
+
     for (let i = 0; i < 5; i++) {
-        $('.board').append(`<div class="cell cell-${i} cell-active">$100</div>`);
+        let cat = showList[i][0].category;
+        cat = cat.replace(/\"/g, "");
+
+        categories.push(cat);
+        $('.board').append(`<div class="cell category">${categories[i]}</div>`);
+    }
+    for (let i = 0; i < 5; i++) {
+        $('.board').append(`<div class="cell cell-${i} cell-active" data-category="${categories[i]}">$200</div>`);
     };
     for (let i = 5; i < 10; i++) {
-        $('.board').append(`<div class="cell cell-${i} cell-active">$200</div>`);
+        $('.board').append(`<div class="cell cell-${i} cell-active" data-category="${categories[i - 5]}">$400</div>`);
     };
     for (let i = 10; i < 15; i++) {
-        $('.board').append(`<div class="cell cell-${i} cell-active">$400</div>`);
+        $('.board').append(`<div class="cell cell-${i} cell-active" data-category="${categories[i - 10]}">$600</div>`);
     };
     for (let i = 15; i < 20; i++) {
-        $('.board').append(`<div class="cell cell-${i} cell-active">$600</div>`);
+        $('.board').append(`<div class="cell cell-${i} cell-active" data-category="${categories[i - 15]}">$800</div>`);
     };
     for (let i = 20; i < 25; i++) {
-        $('.board').append(`<div class="cell cell-${i} cell-active">$800</div>`);
+        $('.board').append(`<div class="cell cell-${i} cell-active" data-category="${categories[i - 20]}">$1,000</div>`);
     };
     for (let cell of pickedCells) {
         $(`.${cell}`).addClass('picked');
@@ -25,33 +39,63 @@ function setup() {
     $('.scoreValue').text(`$${score}`);
     $('#userAnswer').prop('disabled', true);
 
-    if (localStorage.getItem('status') == 'ongoing') {
+    if (localStorage.getItem('status') == 'question') {
         $('.question').text(pickedQuestions[pickedQuestions.length - 1].question);
         $('.question').show();
         $('#userAnswer').prop('disabled', false);
         $('#userAnswer').focus();
-    }
+    };
 };
 
-setup();
+async function getRandomShowNum() {
+    if (!localStorage.getItem('showNum')) {
+        let list = await fetch('jeopardy.json');
+        list = await list.json();
+        let random = Math.floor(Math.random() * list.length);
+        showNum = list[random].showNumber;
 
-async function getQuestion(value) {
-    let questions = await fetch('jeopardy.json');
-    questions = await questions.json();
-    let filtered = questions.filter(x => x.value == value)
-    console.log(filtered)
+        localStorage.setItem('showNum', showNum);
 
-    let random = Math.floor(Math.random() * filtered.length);
+        let filtered = list.filter(x => x.showNumber == showNum);
+        filtered = filtered.filter(x => x.round == 'Jeopardy!');
+        let categories = _.groupBy(filtered, 'category');
+        console.log(categories);
+        let amounts = ['$200', '$400', '$600', '$800', '$1,000'];
+        for (let key in categories) {
+            if (categories[key].length == 5) {
+                const values = categories[key].map(item => item.value);
+                if (amounts.every(amount => values.includes(amount))) {
+                    showList.push(categories[key]);
+                    if (showList.length == 5) break;
+                };
+            };
+        };
+        localStorage.setItem('showList', JSON.stringify(showList));
+    } else {
+        showNum = localStorage.getItem('showNum');
+    };
+};
 
-    let chosenQuestion = filtered[random].question;
-    let chosenAnswer = filtered[random].answer;
-    console.log(random)
+function getQuestion(value, cat) {
+    let chosen;
+    for (let item of showList) {
+        let itemCat = item[0].category;
+        itemCat = itemCat.replace(/\"/g, '');
+        if (itemCat == cat) {
+            chosen = item.find(x => x.value == value);
+            console.log(chosen)
+            break;
+        }
+    }
+
+    let chosenQuestion = chosen.question;
+    let chosenAnswer = chosen.answer;
 
     $('.result').hide();
     $('.question').text(chosenQuestion);
     $('.question').show();
 
-    pickedQuestions.push({ 'question': chosenQuestion, 'answer': chosenAnswer, 'value': value.substring(1) });
+    pickedQuestions.push({ 'question': chosenQuestion, 'answer': chosenAnswer, 'value': value.substring(1).replace(',', '') });
     localStorage.setItem('list', JSON.stringify(pickedQuestions));
 
     $('#userAnswer').prop('disabled', false);
@@ -60,9 +104,9 @@ async function getQuestion(value) {
 
 $('.board').on('click', $('.cell'), function (e) {
     let cellNum = e.target.classList
-    console.log(cellNum)
-    if (localStorage.getItem('status') !== 'ongoing' && !pickedCells.includes(cellNum[1])) {
-        localStorage.setItem('status', 'ongoing');
+
+    if (localStorage.getItem('status') !== 'question' && !pickedCells.includes(cellNum[1]) && $(e.target).hasClass('cell-active')) {
+        localStorage.setItem('status', 'question');
         $('.cell').removeClass('cell-active');
 
         pickedCells.push(cellNum[1]);
@@ -71,7 +115,8 @@ $('.board').on('click', $('.cell'), function (e) {
         $(e.target).addClass('picked');
 
         let value = e.target.textContent;
-        getQuestion(value);
+        let cat = $(e.target).attr('data-category');
+        getQuestion(value, cat);
     };
 });
 
@@ -101,14 +146,9 @@ function checkAnswer() {
     $('.cell').addClass('cell-active');
 };
 
-$('#resetBtn').on('click', function () {
+$('#resetBtn').on('click', resetGame);
+
+function resetGame() {
     localStorage.clear();
-    $('.cell').removeClass('picked');
-    $('.question').hide();
-    $('.result').hide();
-    $('#userAnswer').val('');
-    $('#userAnswer').prop('disabled', true);
-    pickedQuestions = [];
-    pickedCells = [];
-    score = 0;
-});
+    location.reload();
+};
